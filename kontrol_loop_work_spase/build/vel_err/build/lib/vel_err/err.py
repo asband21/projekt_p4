44a25908c55b3b5371e449b0b90f69485af5504a
Shit, whoopin' badung brit <turtlebor@aau.dk>
2023-05-02 13:48:46 +0000

    error works upon can calculate the error from vicone.

--- kontrol_loop_work_spase/build/vel_err/build/lib/vel_err/err.py
+++ kontrol_loop_work_spase/build/vel_err/build/lib/vel_err/err.py
@@ -1,4 +1,5 @@
 import math
+import copy
 import tf2_msgs 
 
 from geometry_msgs.msg import Twist
@@ -12,6 +13,8 @@ from tf2_ros.transform_listener import TransformListener
 
 
 class FrameListener(Node):
+     
+
     def __init__(self):
         super().__init__('oel')
         self.subscription = self.create_subscription(tf2_msgs.msg.TFMessage, 'tf', self.on_timer,10)
@@ -19,15 +22,57 @@ class FrameListener(Node):
         # Declare and acquire `target_frame` parameter
         self.target_frame = self.declare_parameter('vicon', 'Drone').get_parameter_value().string_value
 
+        self.gammel_tf = 0
+
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
         self.get_logger().info("fldfldlf")
 
     def on_timer(self, dd):
+
+        if self.gammel_tf == 0:
+            self.gammel_tf = copy.deepcopy(dd)
+            #dd.transform.translation.x = dd.transform.translation.x+2
+            for tra in self.gammel_tf.transforms:
+                if(tra.child_frame_id == "Drone"):
+                    tra.transform.translation.x = tra.transform.translation.x + 1
+            return 
+
+        vec = [0,0,0,0,0,0,0,0]
+        del_tid = 0
+        for tra in dd.transforms:
+            if(tra.child_frame_id == "Drone"):
+                #self.get_logger().info("x:" + str(tra.transform.translation.x))
+                vec[0] = tra.transform.translation.x
+                vec[1] = tra.transform.translation.y
+                vec[2] = tra.transform.translation.z
+                del_tid = tra.header.stamp
+
+        for tra in self.gammel_tf.transforms:
+            if(tra.child_frame_id == "Drone"):
+                #self.get_logger().info("x:" + str(tra.transform.translation.x))
+                if(del_tid.sec-tra.header.stamp.sec != 0):
+                    del_tid =  del_tid.nanosec - tra.header.stamp.nanosec +1000000000
+                else:
+                    del_tid =  del_tid.nanosec - tra.header.stamp.nanosec
+                vec[4] = (vec[0] - tra.transform.translation.x)/del_tid
+                vec[5] = (vec[1] - tra.transform.translation.y)/del_tid
+                vec[6] = (vec[2] - tra.transform.translation.z)/del_tid
         
-        from_frame_rel = self.target_frame
-        self.get_logger().info(str(dd))
+        self.get_logger().info("vec:" + str(vec))
+        #self.get_logger().info("delta tid:" + str(del_tid))
+        #self.get_logger().info("x:" + str(self.gammel_tf.msg.transform.translation.x))
+        #from_frame_rel = self.target_frame
+        """
+        self.get_logger().info("--------------------------------")
+        self.get_logger().info(str(dd.transforms[0].child_frame_id))
+        self.get_logger().info(str(dd.transforms[0].header.stamp))
+        self.get_logger().info("x:" + str(dd.transforms[0].transform.translation.x))
+        self.get_logger().info("y:" + str(dd.transforms[0].transform.translation.y))
+        self.get_logger().info("z:" + str(dd.transforms[0].transform.translation.z))
+        """
         #self.get_logger().info(f"llll{str(from_frame_rel)}")
+        self.gammel_tf = copy.deepcopy(dd)
 
 def main():
     rclpy.init()
