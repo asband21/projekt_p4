8e3e6ff162fbb42948eaad54cf763eb251286af1
carsten nielsen <cars2109@gmail.com>
2023-05-15 19:24:39 +0200

    image analisys

--- turtlebot3_ws/src/full_system/full_system/Image_analisys.py
+++ turtlebot3_ws/src/full_system/full_system/Image_analisys.py
@@ -49,7 +49,7 @@ class image_analisys(Node):
         #  different resolutions of color and depth streams
         config = rs.config()
 
-        config.enable_stream(rs.stream.depth, 320, 240, rs.format.z16, 30)
+        # config.enable_stream(rs.stream.depth, 320, 240, rs.format.z16, 30)
 
         config.enable_stream(rs.stream.color, 320, 240, rs.format.bgr8, 30)
 
