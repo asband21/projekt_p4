3f0f2c68b28cc620a41bc1edd5374f7625b69583
carsten nielsen <cars2109@gmail.com>
2023-05-21 20:37:53 +0200

    turtle regulation system

--- turtlebot3_ws/src/full_system/full_system/turtle_follower.py
+++ turtlebot3_ws/src/full_system/full_system/turtle_follower.py
@@ -10,51 +10,6 @@ from std_msgs.msg import String
 from geometry_msgs.msg import TransformStamped
 
 
-class PID:
-    """
-    Discrete PID control
-    """
-    def __init__(self, P=0.0, I=0.0, D=0.0, Derivator=0, Integrator=0, Integrator_max=10, Integrator_min=-10):
-        self.Kp = P
-        self.Ki = I
-        self.Kd = D
-        self.Derivator = Derivator
-        self.Integrator = Integrator
-        self.Integrator_max = Integrator_max
-        self.Integrator_min = Integrator_min
-        self.set_point = 0.0
-        self.error = 0.0
-
-    def update(self, current_value):
-        self.error = self.set_point - current_value
-        if self.error > pi:  # specific design for circular situation
-            self.error = self.error - 2*pi
-        elif self.error < -pi:
-            self.error = self.error + 2*pi
-        self.P_value = self.Kp * self.error
-        self.D_value = self.Kd * ( self.error - self.Derivator)
-        self.Derivator = self.error
-        self.Integrator = self.Integrator + self.error
-        if self.Integrator > self.Integrator_max:
-            self.Integrator = self.Integrator_max
-        elif self.Integrator < self.Integrator_min:
-            self.Integrator = self.Integrator_min
-        self.I_value = self.Integrator * self.Ki
-        PID = self.P_value + self.I_value + self.D_value
-        return PID
-
-    def setPoint(self, set_point):
-        self.set_point = set_point
-        self.Derivator = 0
-        self.Integrator = 0
-
-    def setPID(self, set_P=0.0, set_I=0.0, set_D=0.0):
-        self.Kp = set_P
-        self.Ki = set_I
-        self.Kd = set_D
-
-
-
 class turtle_follower(Node):
     def __init__(self):
         super().__init__("turtle_follower")
