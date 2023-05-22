56ff2c2e32d449ab716afe12694e124afe208d33
carsten nielsen <cars2109@gmail.com>
2023-05-22 17:56:16 +0200

    turtle regulation system

--- turtlebot3_ws/src/full_system/full_system/turtle_follower.py
+++ turtlebot3_ws/src/full_system/full_system/turtle_follower.py
@@ -127,6 +127,7 @@ class turtle_follower(Node):
                 angle = angle + 90
             
             while abs(self.get_current_yaw() - angle) > 1:
+                self.get_logger().info("eroror" + str(abs(self.get_current_yaw() - angle)))
                 # self.get_logger().info("angle: " + str(angle))
                 # self.get_logger().info("current_yaw: " + str(self.get_current_yaw()))
                 error = angle - self.get_current_yaw()
@@ -168,6 +169,7 @@ class turtle_follower(Node):
                 angle = angle + 180
 
             while abs(self.get_current_yaw() - angle) > 1:
+                self.get_logger().info("eroror" + str(abs(self.get_current_yaw() - angle)))
                 # self.get_logger().info("angle: " + str(angle))
                 # self.get_logger().info("current_yaw: " + str(self.get_current_yaw()))
                 error = angle - self.get_current_yaw()
