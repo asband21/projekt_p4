cbbf41293fa20bc4bc578aac47ef8454f82fe766
carsten nielsen <cars2109@gmail.com>
2023-05-22 17:28:40 +0200

    turtle regulation system

--- turtlebot3_ws/src/full_system/launch/system.launch.py
+++ turtlebot3_ws/src/full_system/launch/system.launch.py
@@ -24,20 +24,20 @@ def generate_launch_description():
             package='full_system',
             executable='srv_tf',
         ),
-        Node(
-            package='full_system',
-            executable='desired_position',
-        ),
-        Node(
-            package='full_system',
-            executable='qr_tf_pub',
-            # parameters=[{'run_number': LaunchConfiguration('run_number')}]
+        # Node(
+        #     package='full_system',
+        #     executable='desired_position',
+        # ),
+        # Node(
+        #     package='full_system',
+        #     executable='qr_tf_pub',
+        #     # parameters=[{'run_number': LaunchConfiguration('run_number')}]
 
-        ),
-        Node(
-            package='full_system',
-            executable='world_tag_read_tf',
-        ),
+        # ),
+        # # Node(
+        #     package='full_system',
+        #     executable='world_tag_read_tf',
+        # ),
 
         # Node(
         #     package='full_system',
