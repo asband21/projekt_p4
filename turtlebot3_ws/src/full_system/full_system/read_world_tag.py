58493ceff103512d775d80a7f0c5a05805afc1d3
carsten nielsen <cars2109@gmail.com>
2023-05-22 16:23:23 +0200

    turtle regulation system

--- turtlebot3_ws/src/full_system/full_system/read_world_tag.py
+++ turtlebot3_ws/src/full_system/full_system/read_world_tag.py
@@ -3,14 +3,33 @@ import os
 
 # Read data from pickle file and print it
 
+id_to_delete = 5
 
 
 # Open the pickle file
 with open("/home/ubuntu/tests/turtleTest/world_targets.pkl", 'rb') as f:
     data = pickle.load(f)
 
+ids, transforms = data
+
+
+current_data_dict = dict(zip(ids, transforms))
+
+
+for id in ids:
+    if id == id_to_delete:
+        del current_data_dict[id]
+
+
+
+print(current_data_dict)
+
+
 # Print the data
 print(data)
 
 # Print the type of the data
 print(type(data))
+
+
+
